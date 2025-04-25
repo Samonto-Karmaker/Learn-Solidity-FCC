d4b9c05037fcb6bbceeca46eaa8c81b1c56655c5
Samonto Karmaker <samonto0726@gmail.com>
2025-04-25 15:04:22 +0600

    Deploy smart contract to sepolia testnet

--- Hardhat-Simple-Storage/hardhat.config.js
+++ Hardhat-Simple-Storage/hardhat.config.js
@@ -1,6 +1,28 @@
-require("@nomicfoundation/hardhat-toolbox");
-
+// imports
+require("@nomicfoundation/hardhat-toolbox")
+require("@nomicfoundation/hardhat-verify")
+require("dotenv").config()
 /** @type import('hardhat/config').HardhatUserConfig */
+// environment variables
+const SEPOLIA_RPC = process.env.SEPOLIA_ENDPOINT || "TEST_SEPOLIA_RPC"
+const PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY || "PRIVATE_KEY"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "ETHERSCAN_API_KEY"
+// export modules
 module.exports = {
-  solidity: "0.8.28",
-};
+    solidity: "0.8.19",
+    networks: {
+        sepolia: {
+            url: SEPOLIA_RPC,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            // no need for accouts
+            chainId: 31337,
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+}
